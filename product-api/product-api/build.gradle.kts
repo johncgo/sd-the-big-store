b746b28bf1c694050bf216c2edf1a573b19d4e37
Jonathan Oliveira <jonathancgo94@gmail.com>
2024-08-07 21:35:00 -0300

    fix: update project to spring 3 and jdk 17

--- product-api/product-api/build.gradle.kts
+++ product-api/product-api/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
 	java
-	id("org.springframework.boot") version "2.7.10-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "br.com.ufrpe.sd-the-big-store"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
 	compileOnly {
